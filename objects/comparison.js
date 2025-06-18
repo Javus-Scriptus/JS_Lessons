c710e8b46a7c4bb1b84404a47cf059f86f4cb06c
Igor <garikkrass@gmail.com>
2025-06-18 20:12:14 +0200

    comparison

---
+++ objects/comparison.js
@@ -0,0 +1,8 @@
+let rest1 = [1, 2, 3, 4]
+ let rest2 = rest1
+ let identicalRest = [1, 2, 3, 4]
+
+ console.log(rest2) // [1, 2, 3, 4]
+ console.log(identicalRest === rest1) //false
+ console.log(rest1 === rest2) //true
+
